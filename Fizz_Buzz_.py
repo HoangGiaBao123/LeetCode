e0edbeaffc75837762f7ddf14cbea58b1be1f8ec
Gia Bảo <hoanggiabao131012vn@gmail.com>
2026-04-14 17:59:58 +0700

    Create Fizz_Buzz_.py

---
+++ Fizz_Buzz_.py
@@ -0,0 +1,14 @@
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        result = []
+        for n in range(1, n + 1):
+            if n % 3 == 0 and n % 5 == 0:
+                n = 'FizzBuzz'
+            elif n % 3 == 0:
+                n = 'Fizz'
+            elif n % 5 == 0:
+                n = 'Buzz'
+            else:
+                n = str(n)
+            result.append(n)
+        return result
