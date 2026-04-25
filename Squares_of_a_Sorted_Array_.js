242a2ec90df04b7b929743263da4852bd6d00d04
Gia Bảo <hoanggiabao131012vn@gmail.com>
2026-04-25 08:48:54 +0700

    Create Squares_of_a_Sorted_Array_.js

---
+++ Squares_of_a_Sorted_Array_.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortedSquares = function(nums) {
+    const squared = nums.map((n) => {
+        return n ** 2
+    })
+    return squared.sort((x, y) => x - y)
+};
