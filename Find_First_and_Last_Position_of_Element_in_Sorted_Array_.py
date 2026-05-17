6ee8284f4a981bdfc5e7c0124826a72cfba7e661
Gia Bảo <hoanggiabao131012vn@gmail.com>
2026-05-17 09:24:49 +0700

    Update Find_First_and_Last_Position_of_Element_in_Sorted_Array_.py

--- Find_First_and_Last_Position_of_Element_in_Sorted_Array_.py
+++ Find_First_and_Last_Position_of_Element_in_Sorted_Array_.py
@@ -1,15 +1,8 @@
+import bisect
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        if target not in nums:
-            return [-1, -1]
-        else:
-            indexes = []
-            for i, num in enumerate(nums):
-                if num == target:
-                    indexes.append(i)
-                    break
-            for i, num in reversed(list(enumerate(nums))):
-                if num == target:
-                    indexes.append(i)
-                    break
-            return indexes
+        start = bisect.bisect_left(nums, target)
+        end = bisect.bisect_right(nums, target)
+        if start < len(nums) and nums[start] == target:
+            return [start, end - 1]
+        return [-1, -1]
